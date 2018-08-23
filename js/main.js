00c67e5b093dfdd69cb8255f7158a1524e2ec7c7
Penguin8885 <penguin.8885@gmail.com>
2018-08-23 22:39:33 +0900

    add a function to draw the function

--- js/main.js
+++ js/main.js
@@ -5,7 +5,63 @@ function canvas_draw(){
         return false;
     }
     var context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);   //画面クリア
 
+    var func_ary = create_func_ary(1, -10, 10, 200);
+    draw_function(func_ary);
+
+    function draw_function(func_ary){
+        var num = func_ary[0].length;   // 点の数
+        context.beginPath();
+        context.strokeStyle = 'rgb(0, 255, 0)';
+        context.moveTo(func_ary[0][0], func_ary[1][0]);     // 1点目を打つ
+        for(var i = 1; i < num; i++){
+            context.lineTo(func_ary[0][i], func_ary[1][i]); // i点目に向けて線を引く
+        }
+        context.stroke();
+    }
+
+    function draw_blind(crt_x, crt_y){
+        return 0;
+    }
+
+    function create_func_ary(level, x_min, x_max, num_point){
+        var cx_min = canvas.width*0.05;        // キャンバスのx軸の最小値
+        var cx_max = canvas.width*0.95;        // キャンバスのx軸の最大値
+        var cy_min = canvas.height*0.05;       // キャンバスのy軸の最小値
+        var cy_max = canvas.height*0.95;       // キャンバスのy軸の最大値
+
+        /* 関数値の作成 */
+        var ary_x = new Array(num_point); // x座標の配列の用意
+        var ary_y = new Array(num_point); // y座標の配列の用意
+
+        var delta = (x_max - x_min) / num_point;
+        var c_delta = (cx_max - cx_min) / num_point;
+        for(var i = 0; i < num_point; i++){
+            x = x_min + delta*i;            // x_minからi番目のx座標を求める
+            ary_x[i] = cx_min + c_delta*i;  // 描画可能域中でのx座標を求める
+            ary_y[i] = x*x;                 // 関数値を計算
+        }
+
+        /* 関数値を描画可能域に合わせる */
+        var y_min = Math.min.apply(null, ary_y);
+        for(var i = 0; i < num_point; i++){
+            ary_y[i] -= y_min;                  // 関数値の最小値を一旦，0にする
+        }
+        var y_max = Math.max.apply(null, ary_y);
+        var alpha = (cy_max - cy_min) / y_max;  // 倍率
+        for(var i = 0; i < num_point; i++){
+            ary_y[i] *= alpha;                  // 関数の値域の幅を描画可能域幅に
+            ary_y[i] += cy_min;                 // 関数の最小値をcy_minに直す
+        }
+
+        /* 座標系をcanvas座標系に変換 */
+        for(var i = 0; i < num_point; i++){
+            ary_y[i] = canvas.height - ary_y[i] ;
+        }
+
+        return [ary_x, ary_y]; // x,yの値を入れた配列を返却
+    }
 }
 
 function canvas_random_draw(){
